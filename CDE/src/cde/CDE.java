32456ac3522703bc96cda12442ebb2cf3326a378
kaushinew <it19014814@my.sliit.lk>
2021-04-02 12:38:32 +0530

    my test commit 2

---
+++ CDE/src/cde/CDE.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cde;
+
+/**
+ *
+ * @author ASUS
+ */
+public class CDE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("hi there abc");
+    }
+    
+}
